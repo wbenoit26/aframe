c66341e6252b90fd8379910ce75f7804882f6b07
William Benoit <benoi090@umn.edu>
2024-11-14 09:41:24 -0800

    Add multi-rate resampling to time domain supervised

--- projects/train/train/augmentations.py
+++ projects/train/train/augmentations.py
@@ -1,9 +1,10 @@
-from typing import Optional, Union
+from typing import List, Optional, Union
 
 import torch
 from ml4gw import gw
 from ml4gw.distributions import PowerLaw
 from ml4gw.types import BatchTensor
+from torchaudio.transforms import Resample
 
 
 class ChannelSwapper(torch.nn.Module):
@@ -58,6 +59,77 @@ class ChannelMuter(torch.nn.Module):
             X[indices, channel] = torch.zeros(X.shape[-1], device=X.device)
 
         return X, indices
+
+
+class MultiRateResample(torch.nn.Module):
+    """
+    Resample a time series to multiple different sample rates
+
+    Args:
+        original_sample_rate:
+            The sample rate of the original time series in Hz
+        duration:
+            The duration of the original time series in seconds
+        new_sample_rates:
+            A list of new sample rates that different portions
+            of the time series will be resampled to
+        breakpoints:
+            The time at which there is a transition from one
+            sample rate to another
+
+    Returns:
+        A time series Tensor with each of the resampled segments
+        concatenated together
+    """
+
+    def __init__(
+        self,
+        original_sample_rate: int,
+        duration: float,
+        new_sample_rates: List[int],
+        breakpoints: List[float],
+    ):
+        super().__init__()
+        self.original_sample_rate = original_sample_rate
+        self.duration = duration
+        self.new_sample_rates = new_sample_rates
+        self.breakpoints = breakpoints
+        self._validate_inputs()
+
+        # Add endpoints to breakpoint list
+        self.breakpoints.append(duration)
+        self.breakpoints.insert(0, 0)
+
+        self.resamplers = torch.nn.ModuleList(
+            [Resample(original_sample_rate, new) for new in new_sample_rates]
+        )
+        idxs = [
+            [int(breakpoints[i] * new), int(breakpoints[i + 1] * new)]
+            for i, new in enumerate(self.new_sample_rates)
+        ]
+        self.register_buffer("idxs", torch.Tensor(idxs).int())
+
+    def _validate_inputs(self):
+        if len(self.new_sample_rates) != len(self.breakpoints) + 1:
+            raise ValueError(
+                "There are too many/few breakpoints given "
+                "for the number of frequencies"
+            )
+        if max(self.breakpoints) >= self.duration:
+            raise ValueError(
+                "At least one breakpoint was greater than the given duration"
+            )
+        if not self.breakpoints[1:] > self.breakpoints[:-1]:
+            raise ValueError("Breakpoints must be sorted in ascending order")
+
+    def forward(self, X: torch.Tensor):
+        return torch.cat(
+            [
+                resample(X)[..., idx[0] : idx[1]]
+                for resample, idx in zip(self.resamplers, self.idxs)
+            ],
+            dim=-1,
+        )
 
 
 class SnrRescaler(torch.nn.Module):
--- projects/train/train/data/supervised/time_domain.py
+++ projects/train/train/data/supervised/time_domain.py
@@ -1,9 +1,30 @@
+from typing import List, Optional
+
 import torch
 
+from train.augmentations import MultiRateResample
 from train.data.supervised.supervised import SupervisedAframeDataset
 
 
 class TimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
+    def __init__(
+        self,
+        *args,
+        new_sample_rates: Optional[List[int]] = None,
+        breakpoints: Optional[List[float]] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        if new_sample_rates is not None and breakpoints is not None:
+            self.resampler = MultiRateResample(
+                original_sample_rate=self.hparams.sample_rate,
+                duration=self.hparams.kernel_length,
+                new_sample_rates=new_sample_rates,
+                breakpoints=breakpoints,
+            )
+        else:
+            self.resampler = None
+
     def build_val_batches(self, background, signals):
         X_bg, X_inj, psds = super().build_val_batches(background, signals)
         X_bg = self.whitener(X_bg, psds)
@@ -14,9 +35,14 @@ class TimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
             X_fg.append(inj)
 
         X_fg = torch.stack(X_fg)
+        if self.resampler is not None:
+            X_bg = self.resampler(X_bg)
+            X_fg = self.resampler(X_fg)
         return X_bg, X_fg
 
     def augment(self, X, waveforms):
         X, y, psds = super().augment(X, waveforms)
         X = self.whitener(X, psds)
+        if self.resampler is not None:
+            X = self.resampler(X)
         return X, y
